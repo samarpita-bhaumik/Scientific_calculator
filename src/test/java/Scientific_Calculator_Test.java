f7ff55ca9b042ad1777b8f160f7ce6b4c0f1ed5c
samarpita-bhaumik <54763781+samarpita-bhaumik@users.noreply.github.com>
2024-02-24 12:56:06 +0530

    Update

--- src/test/java/Scientific_Calculator_Test.java
+++ src/test/java/Scientific_Calculator_Test.java
@@ -10,7 +10,7 @@ public class Scientific_Calculator_Test {
 
     @Test
     public void testSquareRootFunc() {
-        assertEquals("Value not Correct", 4, ob.squareRoot(16), 0);
+        assertEquals("Square root of16 should be 4", 4, ob.squareRoot(16), 0);
         assertEquals("Square root of 25 should be 5", 5, ob.squareRoot(25), 0);
         assertEquals("Square root of 36 should be 6", 6, ob.squareRoot(36), 0);
     }
@@ -22,7 +22,7 @@ public class Scientific_Calculator_Test {
 
     @Test
     public void testFactorialFunc() {
-        assertEquals("Value not Correct",120, ob.factorial(5),0);
+        assertEquals("Factorial of 5 should be 120",120, ob.factorial(5),0);
         assertEquals("Factorial of 6 should be 720", 720, ob.factorial(6), 0);
         assertEquals("Factorial of 10 should be 3628800", 3628800, ob.factorial(10), 0);
     }
@@ -32,7 +32,7 @@ public class Scientific_Calculator_Test {
     }
     @Test
     public void testLogNatural() {
-        assertEquals("Value not Correct",2.772588722239781, ob.naturalLogFun(16),0);
+        assertEquals("Natural logarithm of 16 should be approximately 2.772588722239781",2.772588722239781, ob.naturalLogFun(16),0);
         assertEquals("Natural logarithm of 25 should be approximately 3.2188758248682006", 3.2188758248682006, ob.naturalLogFun(25), 0.000000000000001);
         assertEquals("Natural logarithm of 36 should be approximately 3.58351893845611", 3.58351893845611, ob.naturalLogFun(36), 0.00000000000001);
     }
@@ -41,7 +41,7 @@ public class Scientific_Calculator_Test {
 
     @Test
     public void testPowerFunc() {
-        assertEquals("Value not Correct",16, ob.powerFunc(2,4),0);
+        assertEquals("2 raised to the power of 4 should be 16",16, ob.powerFunc(2,4),0);
         assertEquals("3 raised to the power of 3 should be 27", 27, ob.powerFunc(3, 3), 0);
         assertEquals("5 raised to the power of 2 should be 25", 25, ob.powerFunc(5, 2), 0);
     }
